709af599b3e6bddde4a7de16e5dd5438d13b6ce8
macbook <ma4shitm@gmail.com>
2024-10-26 16:08:58 +0200

    zad2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 #   Zadanie 1. Problem Podziału Paczek (Prograowanie Proceduralne)
 #       Mamy listę paczek o różnych wagach oraz maksymalną wagę, jaką może unieść kurier w jednym kursie.
 #       Twoim zadaniem jest podzielić paczki na jak najmniejszą liczbę kursów, aby każda waga nie przekraczała
@@ -7,7 +9,6 @@
 #           • Napisz funkcję, która przyjmuje listę wag paczek i maksymalną wagę.
 #           • Użyj pętli for i instrukcji warunkowych if, else do iteracyjnego podziału paczek.
 #           • Program powinien zwracać minimalną liczbę kursów oraz listę paczek w każdym kursie.
-
 
 def getMinimamalCoursesWithPackages(packagesWeightList, maxWeight):
     for package in packagesWeightList:
@@ -32,6 +33,53 @@ maxWeight = 20
 
 coursesNumber, courses = getMinimamalCoursesWithPackages(packages, maxWeight)
 
-for i, course in enumerate(courses, 1):
-    print(f" Course {i}: {course} - Sum of weights {sum(course)}")
+# for i, course in enumerate(courses, 1):
+#     print(f" Course {i}: {course} - Sum of weights {sum(course)}")
 
+#   Zadanie 2. Wyznaczanie Najkrótszej Ścieżki (Programowanie Funkcyjne)
+#   Stwórz program obliczający najkrótszą ścieżkę w grafie nieważonym przy użyciu algorytmu BFS
+#   (Breadth-First Search). Implementacja powinna być zrealizowana przy użyciu programowania
+#   funkcyjnego z naciskiem na niemutowalne struktury danych, funkcje lambda, i mapowanie.
+#   Wymagania:
+#         • Napisz funkcję BFS przy użyciu funkcyjnego podejścia z rekurencją lub funkcjami wyższego
+#            rzędu.
+#         • Zaimplementuj graf jako słownik (dict), gdzie klucz to wierzchołek, a wartość to lista sąsiednich
+#           wierzchołków.
+#         • Funkcja powinna przyjmować graf oraz wierzchołek początkowy i końcowy, zwracając
+#           najkrótszą ścieżkę jako listę wierzchołków.
+
+
+def bfs(graph, start, end):
+    queue = deque([start])
+    visited = set([start])
+    predecessors = {start: None}
+
+    while queue:
+        node = queue.popleft()
+
+        if node == end:
+            path = []
+            while node is not None:
+                path.append(node)
+                node = predecessors[node]
+            path.reverse()
+            return path
+
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+                predecessors[neighbour] = node
+
+    return None
+
+exampleGraph = {
+    'A': ['B', 'D'],
+    'B': ['A', 'D', 'C'],
+    'C': ['B', 'F'],
+    'D': ['B', 'A', 'E', 'F'],
+    'E': ['D', 'F'],
+    'F': ['D', 'E', 'C']
+}
+
+print (bfs(exampleGraph, 'F', 'B'))
